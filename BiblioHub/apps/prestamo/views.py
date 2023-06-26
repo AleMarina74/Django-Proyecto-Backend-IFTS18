8f5ee9c7fb769f936dcfc9b64b4fee54c0d83df0
AleMarina74 <ahilenrocio@gmail.com>
2023-06-26 19:05:08 -0300

    depurando mas

--- BiblioHub/apps/prestamo/views.py
+++ BiblioHub/apps/prestamo/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.urls import reverse_lazy
 from django.utils.timezone import now
 from .models import Prestamo
